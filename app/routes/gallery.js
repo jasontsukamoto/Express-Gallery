7ec86795ce3803930c8b6a96e1c9cc5075deb8a0
jasontsukamoto <jasontsukamoto@gmail.com>
2015-07-18 15:43:13 -1000

    adds ability to get photo by id

--- app/routes/gallery.js
+++ app/routes/gallery.js
@@ -3,13 +3,37 @@
 //  if there is only one path, don't use a modulei
 var express = require('express');
 var router = express.Router();
+var db = require('../../models');
 
 router.post('/', function(req, res) {
-  res.send('new picture posted!');
+  res.end();
+  // db.Picture.findAll({ where : {
+  //                       id :
+  // }})
 });
 
+
+
+
+
+  // db.Picture.findAll().then(function(pictures) {
+  //   console.log(pictures);
+  //   res.render('contact.jade', { pictures : pictures });
+  // });
+
+
+
 router.get('/:id', function(req, res) {
-  res.send('gallery photo');
+  // var id = req.url.split('').splice(1).join('');
+  var id = req.params.id;
+  db.Picture.find({ where : {
+                        id : id
+                      }
+  }).then(function(picture) {
+    res.render('gallery.jade', { picture : picture });
+  });
+
+  // res.send('gallery photo');
 });
 
 router.get('/:id/edit', function(req, res) {
